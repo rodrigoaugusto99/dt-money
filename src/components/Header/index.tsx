60e5ba7a9cb31b0442282a97c6c874bb041c4d7f
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-03-11 00:59:47 -0300

    modal with Radix UI

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-
+import * as Dialog from '@radix-ui/react-dialog';
 import logoImg from "../../assets/logo.svg";
 
 export function Header() {
@@ -9,6 +9,22 @@ export function Header() {
         <img src={logoImg} alt="" />
 
         <NewTransactionButton>Nova transação</NewTransactionButton>
+
+        <Dialog.Root>
+        <Dialog.Trigger asChild>
+          <NewTransactionButton>Nova transação</NewTransactionButton>
+        </Dialog.Trigger>
+
+        <Dialog.Portal>
+          <Dialog.Overlay />
+
+          <Dialog.Content>
+            <Dialog.Title>Nova Transação</Dialog.Title>
+
+            <Dialog.Close />
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
