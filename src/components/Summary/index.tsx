0b14278685cd6deb0cfac0a8a7ffcbe157343ead
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-03-17 22:53:25 -0300

    criando um hook para o summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,35 +1,16 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import { SummaryCard, SummaryContainer } from "./styles";
-import { useContext } from "react";
-import { TransactionsContext } from "../../contexts/TransactionsContext";
+
 import { priceFormatter } from '../../utils/formatter';
+import { useSummary } from "../../hooks/useSummary";
 
 export function Summary() {
   /*o header de cada item eh a parte de cima, que tem 
   o texto e o icone em row 
   
   span pra poder estilizar a tag que ta em volta do texto*/
-
-  const { transactions } = useContext(TransactionsContext);
-
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price;
-        acc.total += transaction.price;
-      } else {
-        acc.outcome += transaction.price;
-        acc.total -= transaction.price;
-      }
-
-      return acc;
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  );
+const summary = useSummary()
+  
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -47,7 +28,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>{priceFormatter.format(summary.income)}</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
