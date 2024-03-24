29abb31a0930275c547fa93e5714f5ab48dac598
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-03-23 23:28:42 -0300

    addÇ useMemo

--- src/hooks/useSummary.tsx
+++ src/hooks/useSummary.tsx
@@ -1,12 +1,14 @@
 import { useContextSelector } from 'use-context-selector'
 import { TransactionsContext } from '../contexts/TransactionsContext'
+import { useMemo } from 'react'
 
 export function useSummary() {
   const transactions = useContextSelector(TransactionsContext, (context) => {
     return context.transactions
   })
 
-  const summary = transactions.reduce(
+  const summary = useMemo(() => {
+    return transactions.reduce(
     (acc, transaction) => {
       if (transaction.type === 'income') {
         acc.income += transaction.price
@@ -24,6 +26,6 @@ export function useSummary() {
       total: 0,
     },
   )
-
+}, [transactions])
   return summary
 }
